817308639aa0a3ef86923a88f7750f9cbd0cfe86
mcyph <20507948+mcyph@users.noreply.github.com>
2021-03-19 21:50:55 +1100

    started updating editor code to new js syntax

--- javascript/src/js/editor/mxDefaultKeyHandler.js
+++ javascript/src/js/editor/mxDefaultKeyHandler.js
@@ -2,6 +2,10 @@
  * Copyright (c) 2006-2015, JGraph Ltd
  * Copyright (c) 2006-2015, Gaudenz Alder
  */
+
+import mxKeyHandler from "../handler/mxKeyHandler";
+import mxEventObject from "../util/mxEventObject";
+
 /**
  * Class: mxDefaultKeyHandler
  *
@@ -10,117 +14,111 @@
  * only cancel the editing, but also hide the properties dialog and fire an
  * <mxEditor.escape> event via <editor>. An instance of this class is created
  * by <mxEditor> and stored in <mxEditor.keyHandler>.
- * 
+ *
  * Example:
- * 
+ *
  * Bind the delete key to the delete action in an existing editor.
- * 
+ *
  * (code)
- * var keyHandler = new mxDefaultKeyHandler(editor);
+ * let keyHandler = new mxDefaultKeyHandler(editor);
  * keyHandler.bindAction(46, 'delete');
  * (end)
  *
  * Codec:
- * 
+ *
  * This class uses the <mxDefaultKeyHandlerCodec> to read configuration
  * data into an existing instance. See <mxDefaultKeyHandlerCodec> for a
  * description of the configuration format.
- * 
+ *
  * Keycodes:
- * 
+ *
  * See <mxKeyHandler>.
- * 
+ *
  * An <mxEvent.ESCAPE> event is fired via the editor if the escape key is
  * pressed.
- * 
+ *
  * Constructor: mxDefaultKeyHandler
  *
  * Constructs a new default key handler for the <mxEditor.graph> in the
  * given <mxEditor>. (The editor may be null if a prototypical instance for
  * a <mxDefaultKeyHandlerCodec> is created.)
- * 
+ *
  * Parameters:
- * 
+ *
  * editor - Reference to the enclosing <mxEditor>.
  */
-function mxDefaultKeyHandler(editor)
-{
-	if (editor != null)
-	{
-		this.editor = editor;
-		this.handler = new mxKeyHandler(editor.graph);
-		
-		// Extends the escape function of the internal key
-		// handle to hide the properties dialog and fire
-		// the escape event via the editor instance
-		var old = this.handler.escape;
-		
-		this.handler.escape = function(evt)
-		{
-			old.apply(this, arguments);
-			editor.hideProperties();
-			editor.fireEvent(new mxEventObject(mxEvent.ESCAPE, 'event', evt));
-		};
-	}
-};
-	
-/**
- * Variable: editor
- *
- * Reference to the enclosing <mxEditor>.
- */
-mxDefaultKeyHandler.prototype.editor = null;
+class mxDefaultKeyHandler {
+  /**
+   * Variable: editor
+   *
+   * Reference to the enclosing <mxEditor>.
+   */
+  editor = null;
 
-/**
- * Variable: handler
- *
- * Holds the <mxKeyHandler> for key event handling.
- */
-mxDefaultKeyHandler.prototype.handler = null;
+  /**
+   * Variable: handler
+   *
+   * Holds the <mxKeyHandler> for key event handling.
+   */
+  handler = null;
 
-/**
- * Function: bindAction
- *
- * Binds the specified keycode to the given action in <editor>. The
- * optional control flag specifies if the control key must be pressed
- * to trigger the action.
- *
- * Parameters:
- *
- * code - Integer that specifies the keycode.
- * action - Name of the action to execute in <editor>.
- * control - Optional boolean that specifies if control must be pressed.
- * Default is false.
- */
-mxDefaultKeyHandler.prototype.bindAction = function (code, action, control)
-{
-	var keyHandler = mxUtils.bind(this, function()
-	{
-		this.editor.execute(action);
-	});
+  constructor(editor) {
+    if (editor != null) {
+      this.editor = editor;
+      this.handler = new mxKeyHandler(editor.graph);
 
-	// Binds the function to control-down keycode
-	if (control)
-	{
-		this.handler.bindControlKey(code, keyHandler);
-	}
+      // Extends the escape function of the internal key
+      // handle to hide the properties dialog and fire
+      // the escape event via the editor instance
+      let old = this.handler.escape;
 
-	// Binds the function to the normal keycode
-	else
-	{
-		this.handler.bindKey(code, keyHandler);				
-	}
-};
+      this.handler.escape = (evt)=>{
+        old.apply(this, arguments);
+        editor.hideProperties();
+        editor.fireEvent(new mxEventObject(mxEvent.ESCAPE, 'event', evt));
+      };
+    }
+  };
 
-/**
- * Function: destroy
- *
- * Destroys the <handler> associated with this object. This does normally
- * not need to be called, the <handler> is destroyed automatically when the
- * window unloads (in IE) by <mxEditor>.
- */
-mxDefaultKeyHandler.prototype.destroy = function ()
-{
-	this.handler.destroy();
-	this.handler = null;
-};
+  /**
+   * Function: bindAction
+   *
+   * Binds the specified keycode to the given action in <editor>. The
+   * optional control flag specifies if the control key must be pressed
+   * to trigger the action.
+   *
+   * Parameters:
+   *
+   * code - Integer that specifies the keycode.
+   * action - Name of the action to execute in <editor>.
+   * control - Optional boolean that specifies if control must be pressed.
+   * Default is false.
+   */
+  bindAction=(code, action, control)=>{
+    let keyHandler = mxUtils.bind(this, ()=>{
+      this.editor.execute(action);
+    });
+
+    if (control) {
+      // Binds the function to control-down keycode
+      this.handler.bindControlKey(code, keyHandler);
+    } else {
+      // Binds the function to the normal keycode
+      this.handler.bindKey(code, keyHandler);
+    }
+  };
+
+  /**
+   * Function: destroy
+   *
+   * Destroys the <handler> associated with this object. This does normally
+   * not need to be called, the <handler> is destroyed automatically when the
+   * window unloads (in IE) by <mxEditor>.
+   */
+  destroy=()=>{
+    this.handler.destroy();
+    this.handler = null;
+  };
+}
+
+export default mxDefaultKeyHandler;
