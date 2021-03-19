fd21dbe3d9d25c83d9b458b6e51261c71b62a243
mcyph <20507948+mcyph@users.noreply.github.com>
2021-03-20 00:22:34 +1100

    manual js syntax updates

--- javascript/src/js/shape/mxConnector.js
+++ javascript/src/js/shape/mxConnector.js
@@ -2,148 +2,137 @@
  * Copyright (c) 2006-2015, JGraph Ltd
  * Copyright (c) 2006-2015, Gaudenz Alder
  */
-/**
- * Class: mxConnector
- * 
- * Extends <mxShape> to implement a connector shape. The connector
- * shape allows for arrow heads on either side.
- * 
- * This shape is registered under <mxConstants.SHAPE_CONNECTOR> in
- * <mxCellRenderer>.
- * 
- * Constructor: mxConnector
- * 
- * Constructs a new connector shape.
- * 
- * Parameters:
- * 
- * points - Array of <mxPoints> that define the points. This is stored in
- * <mxShape.points>.
- * stroke - String that defines the stroke color. This is stored in <stroke>.
- * Default is 'black'.
- * strokewidth - Optional integer that defines the stroke width. Default is
- * 1. This is stored in <strokewidth>.
- */
-function mxConnector(points, stroke, strokewidth)
-{
-  mxPolyline.call(this, points, stroke, strokewidth);
-};
 
-/**
- * Extends mxPolyline.
- */
-mxUtils.extend(mxConnector, mxPolyline);
-
-/**
- * Function: updateBoundingBox
- *
- * Updates the <boundingBox> for this shape using <createBoundingBox> and
- * <augmentBoundingBox> and stores the result in <boundingBox>.
- */
-updateBoundingBox = ()=>
-{
-  this.useSvgBoundingBox = this.style != null && this.style[mxConstants.STYLE_CURVED] == 1;
-  updateBoundingBox.apply(this, arguments);
-};
-
-/**
- * Function: paintEdgeShape
- * 
- * Paints the line shape.
- */
-paintEdgeShape = (c, pts)=>
-{
-  // The indirection via functions for markers is needed in
-  // order to apply the offsets before painting the line and
-  // paint the markers after painting the line.
-  var sourceMarker = this.createMarker(c, pts, true);
-  var targetMarker = this.createMarker(c, pts, false);
-
-  paintEdgeShape.apply(this, arguments);
-
-  // Disables shadows, dashed styles and fixes fill color for markers
-  c.setFillColor(this.stroke);
-  c.setShadow(false);
-  c.setDashed(false);
-
-  if (sourceMarker != null)
-  {
-    sourceMarker();
+class mxConnector extends mxPolyLine {
+  /**
+   * Class: mxConnector
+   *
+   * Extends <mxShape> to implement a connector shape. The connector
+   * shape allows for arrow heads on either side.
+   *
+   * This shape is registered under <mxConstants.SHAPE_CONNECTOR> in
+   * <mxCellRenderer>.
+   *
+   * Constructor: mxConnector
+   *
+   * Constructs a new connector shape.
+   *
+   * Parameters:
+   *
+   * points - Array of <mxPoints> that define the points. This is stored in
+   * <mxShape.points>.
+   * stroke - String that defines the stroke color. This is stored in <stroke>.
+   * Default is 'black'.
+   * strokewidth - Optional integer that defines the stroke width. Default is
+   * 1. This is stored in <strokewidth>.
+   */
+  constructor(points, stroke, strokewidth) {
+    super(points, stroke, strokewidth);
   }
 
-  if (targetMarker != null)
-  {
-    targetMarker();
-  }
-};
+  /**
+   * Function: updateBoundingBox
+   *
+   * Updates the <boundingBox> for this shape using <createBoundingBox> and
+   * <augmentBoundingBox> and stores the result in <boundingBox>.
+   */
+  updateBoundingBox = () => {
+    this.useSvgBoundingBox = this.style != null && this.style[mxConstants.STYLE_CURVED] === 1;
+    updateBoundingBox.apply(this, arguments);
+  };
 
-/**
- * Function: createMarker
- * 
- * Prepares the marker by adding offsets in pts and returning a function to
- * paint the marker.
- */
-createMarker = (c, pts, source)=>
-{
-  var result = null;
-  var n = pts.length;
-  var type = mxUtils.getValue(this.style, (source) ? mxConstants.STYLE_STARTARROW : mxConstants.STYLE_ENDARROW);
-  var p0 = (source) ? pts[1] : pts[n - 2];
-  var pe = (source) ? pts[0] : pts[n - 1];
+  /**
+   * Function: paintEdgeShape
+   *
+   * Paints the line shape.
+   */
+  paintEdgeShape = (c, pts) => {
+    // The indirection via functions for markers is needed in
+    // order to apply the offsets before painting the line and
+    // paint the markers after painting the line.
+    var sourceMarker = this.createMarker(c, pts, true);
+    var targetMarker = this.createMarker(c, pts, false);
 
-  if (type != null && p0 != null && pe != null)
-  {
-    var count = 1;
+    super.paintEdgeShape(c, pts);
 
-    // Uses next non-overlapping point
-    while (count < n - 1 && Math.round(p0.x - pe.x) == 0 && Math.round(p0.y - pe.y) == 0)
-    {
-      p0 = (source) ? pts[1 + count] : pts[n - 2 - count];
-      count++;
+    // Disables shadows, dashed styles and fixes fill color for markers
+    c.setFillColor(this.stroke);
+    c.setShadow(false);
+    c.setDashed(false);
+
+    if (sourceMarker != null) {
+      sourceMarker();
     }
 
-    // Computes the norm and the inverse norm
-    var dx = pe.x - p0.x;
-    var dy = pe.y - p0.y;
+    if (targetMarker != null) {
+      targetMarker();
+    }
+  };
 
-    var dist = Math.max(1, Math.sqrt(dx * dx + dy * dy));
+  /**
+   * Function: createMarker
+   *
+   * Prepares the marker by adding offsets in pts and returning a function to
+   * paint the marker.
+   */
+  createMarker = (c, pts, source) => {
+    var result = null;
+    var n = pts.length;
+    var type = mxUtils.getValue(this.style, (source) ? mxConstants.STYLE_STARTARROW : mxConstants.STYLE_ENDARROW);
+    var p0 = (source) ? pts[1] : pts[n - 2];
+    var pe = (source) ? pts[0] : pts[n - 1];
 
-    var unitX = dx / dist;
-    var unitY = dy / dist;
+    if (type != null && p0 != null && pe != null) {
+      var count = 1;
 
-    var size = mxUtils.getNumber(this.style, (source) ? mxConstants.STYLE_STARTSIZE : mxConstants.STYLE_ENDSIZE, mxConstants.DEFAULT_MARKERSIZE);
+      // Uses next non-overlapping point
+      while (count < n - 1 && Math.round(p0.x - pe.x) === 0 && Math.round(p0.y - pe.y) === 0) {
+        p0 = (source) ? pts[1 + count] : pts[n - 2 - count];
+        count++;
+      }
 
-    // Allow for stroke width in the end point used and the
-    // orthogonal vectors describing the direction of the marker
-    var filled = this.style[(source) ? mxConstants.STYLE_STARTFILL : mxConstants.STYLE_ENDFILL] != 0;
+      // Computes the norm and the inverse norm
+      var dx = pe.x - p0.x;
+      var dy = pe.y - p0.y;
 
-    result = mxMarker.createMarker(c, this, type, pe, unitX, unitY, size, source, this.strokewidth, filled);
-  }
+      var dist = Math.max(1, Math.sqrt(dx * dx + dy * dy));
 
-  return result;
-};
+      var unitX = dx / dist;
+      var unitY = dy / dist;
 
-/**
- * Function: augmentBoundingBox
- *
- * Augments the bounding box with the strokewidth and shadow offsets.
- */
-augmentBoundingBox = (bbox)=>
-{
-  augmentBoundingBox.apply(this, arguments);
+      var size = mxUtils.getNumber(this.style, (source) ? mxConstants.STYLE_STARTSIZE : mxConstants.STYLE_ENDSIZE, mxConstants.DEFAULT_MARKERSIZE);
 
-  // Adds marker sizes
-  var size = 0;
+      // Allow for stroke width in the end point used and the
+      // orthogonal vectors describing the direction of the marker
+      var filled = this.style[(source) ? mxConstants.STYLE_STARTFILL : mxConstants.STYLE_ENDFILL] !== 0;
 
-  if (mxUtils.getValue(this.style, mxConstants.STYLE_STARTARROW, mxConstants.NONE) != mxConstants.NONE)
-  {
-    size = mxUtils.getNumber(this.style, mxConstants.STYLE_STARTSIZE, mxConstants.DEFAULT_MARKERSIZE) + 1;
-  }
+      result = mxMarker.createMarker(c, this, type, pe, unitX, unitY, size, source, this.strokewidth, filled);
+    }
 
-  if (mxUtils.getValue(this.style, mxConstants.STYLE_ENDARROW, mxConstants.NONE) != mxConstants.NONE)
-  {
-    size = Math.max(size, mxUtils.getNumber(this.style, mxConstants.STYLE_ENDSIZE, mxConstants.DEFAULT_MARKERSIZE)) + 1;
-  }
+    return result;
+  };
 
-  bbox.grow(size * this.scale);
-};
+  /**
+   * Function: augmentBoundingBox
+   *
+   * Augments the bounding box with the strokewidth and shadow offsets.
+   */
+  augmentBoundingBox = (bbox) => {
+    super.augmentBoundingBox(bbox);
+
+    // Adds marker sizes
+    var size = 0;
+
+    if (mxUtils.getValue(this.style, mxConstants.STYLE_STARTARROW, mxConstants.NONE) !== mxConstants.NONE) {
+      size = mxUtils.getNumber(this.style, mxConstants.STYLE_STARTSIZE, mxConstants.DEFAULT_MARKERSIZE) + 1;
+    }
+
+    if (mxUtils.getValue(this.style, mxConstants.STYLE_ENDARROW, mxConstants.NONE) !== mxConstants.NONE) {
+      size = Math.max(size, mxUtils.getNumber(this.style, mxConstants.STYLE_ENDSIZE, mxConstants.DEFAULT_MARKERSIZE)) + 1;
+    }
+
+    bbox.grow(size * this.scale);
+  };
+}
+
+export default mxConnector;
--- javascript/src/js/shape/mxTriangle.js
+++ javascript/src/js/shape/mxTriangle.js
@@ -2,42 +2,41 @@
  * Copyright (c) 2006-2015, JGraph Ltd
  * Copyright (c) 2006-2015, Gaudenz Alder
  */
-/**
- * Class: mxTriangle
- * 
- * Implementation of the triangle shape.
- * 
- * Constructor: mxTriangle
- *
- * Constructs a new triangle shape.
- */
-function mxTriangle()
-{
-  mxActor.call(this);
-};
 
-/**
- * Extends mxActor.
- */
-mxUtils.extend(mxTriangle, mxActor);
+import mxPoint from "FIXME";
 
-/**
- * Function: isRoundable
- * 
- * Adds roundable support.
- */
-isRoundable = ()=>
-{
-  return true;
-};
+class mxTriangle extends mxActor {
+  /**
+   * Class: mxTriangle
+   *
+   * Implementation of the triangle shape.
+   *
+   * Constructor: mxTriangle
+   *
+   * Constructs a new triangle shape.
+   */
+  constructor() {
+    super();
+  }
 
-/**
- * Function: redrawPath
- *
- * Draws the path for this shape.
- */
-redrawPath = (c, x, y, w, h)=>
-{
-  var arcSize = mxUtils.getValue(this.style, mxConstants.STYLE_ARCSIZE, mxConstants.LINE_ARCSIZE) / 2;
-  this.addPoints(c, [new mxPoint(0, 0), new mxPoint(w, 0.5 * h), new mxPoint(0, h)], this.isRounded, arcSize, true);
-};
+  /**
+   * Function: isRoundable
+   *
+   * Adds roundable support.
+   */
+  isRoundable = () => {
+    return true;
+  };
+
+  /**
+   * Function: redrawPath
+   *
+   * Draws the path for this shape.
+   */
+  redrawPath = (c, x, y, w, h) => {
+    var arcSize = mxUtils.getValue(this.style, mxConstants.STYLE_ARCSIZE, mxConstants.LINE_ARCSIZE) / 2;
+    this.addPoints(c, [new mxPoint(0, 0), new mxPoint(w, 0.5 * h), new mxPoint(0, h)], this.isRounded, arcSize, true);
+  };
+}
+
+export default mxTriangle;
