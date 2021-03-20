a599a51ef9eda83f6b8feeb36f1fb75ffdb5c2f3
mcyph <20507948+mcyph@users.noreply.github.com>
2021-03-20 16:06:14 +1100

    manual updates to js syntax

--- src/js/handler/mxCellHighlight.js
+++ src/js/handler/mxCellHighlight.js
@@ -2,294 +2,265 @@
  * Copyright (c) 2006-2015, JGraph Ltd
  * Copyright (c) 2006-2015, Gaudenz Alder
  */
-/**
- * Class: mxCellHighlight
- * 
- * A helper class to highlight cells. Here is an example for a given cell.
- * 
- * (code)
- * var highlight = new mxCellHighlight(graph, '#ff0000', 2);
- * highlight.highlight(graph.view.getState(cell)));
- * (end)
- * 
- * Constructor: mxCellHighlight
- * 
- * Constructs a cell highlight.
- */
-function mxCellHighlight(graph, highlightColor, strokeWidth, dashed)
-{
-  if (graph != null)
-  {
-    this.graph = graph;
-    this.highlightColor = (highlightColor != null) ? highlightColor : mxConstants.DEFAULT_VALID_COLOR;
-    this.strokeWidth = (strokeWidth != null) ? strokeWidth : mxConstants.HIGHLIGHT_STROKEWIDTH;
-    this.dashed = (dashed != null) ? dashed : false;
-    this.opacity = mxConstants.HIGHLIGHT_OPACITY;
 
-    // Updates the marker if the graph changes
-    this.repaintHandler = mxUtils.bind(this, ()=>{
-    {
-      // Updates reference to state
-      if (this.state != null)
-      {
-        var tmp = this.graph.view.getState(this.state.cell);
-        
-        if (tmp == null)
-        {
-          this.hide();
+class mxCellHighlight {
+  /**
+   * Variable: keepOnTop
+   *
+   * Specifies if the highlights should appear on top of everything
+   * else in the overlay pane. Default is false.
+   */
+  keepOnTop = false;
+
+  /**
+   * Variable: graph
+   *
+   * Reference to the enclosing <mxGraph>.
+   */
+  graph = null;
+
+  /**
+   * Variable: state
+   *
+   * Reference to the <mxCellState>.
+   */
+  state = null;
+
+  /**
+   * Variable: spacing
+   *
+   * Specifies the spacing between the highlight for vertices and the vertex.
+   * Default is 2.
+   */
+  spacing = 2;
+
+  /**
+   * Variable: resetHandler
+   *
+   * Holds the handler that automatically invokes reset if the highlight
+   * should be hidden.
+   */
+  resetHandler = null;
+
+  /**
+   * Class: mxCellHighlight
+   *
+   * A helper class to highlight cells. Here is an example for a given cell.
+   *
+   * (code)
+   * var highlight = new mxCellHighlight(graph, '#ff0000', 2);
+   * highlight.highlight(graph.view.getState(cell)));
+   * (end)
+   *
+   * Constructor: mxCellHighlight
+   *
+   * Constructs a cell highlight.
+   */
+  constructor(graph, highlightColor, strokeWidth, dashed) {
+    if (graph != null) {
+      this.graph = graph;
+      this.highlightColor = (highlightColor != null) ? highlightColor : mxConstants.DEFAULT_VALID_COLOR;
+      this.strokeWidth = (strokeWidth != null) ? strokeWidth : mxConstants.HIGHLIGHT_STROKEWIDTH;
+      this.dashed = (dashed != null) ? dashed : false;
+      this.opacity = mxConstants.HIGHLIGHT_OPACITY;
+
+      // Updates the marker if the graph changes
+      this.repaintHandler = () => {
+        // Updates reference to state
+        if (this.state != null) {
+          var tmp = this.graph.view.getState(this.state.cell);
+
+          if (tmp == null) {
+            this.hide();
+          } else {
+            this.state = tmp;
+            this.repaint();
+          }
         }
-        else
-        {
-          this.state = tmp;
-          this.repaint();
-        }
+      };
+
+      this.graph.getView().addListener(mxEvent.SCALE, this.repaintHandler);
+      this.graph.getView().addListener(mxEvent.TRANSLATE, this.repaintHandler);
+      this.graph.getView().addListener(mxEvent.SCALE_AND_TRANSLATE, this.repaintHandler);
+      this.graph.getModel().addListener(mxEvent.CHANGE, this.repaintHandler);
+
+      // Hides the marker if the current root changes
+      this.resetHandler = () => {
+        this.hide();
+      };
+
+      this.graph.getView().addListener(mxEvent.DOWN, this.resetHandler);
+      this.graph.getView().addListener(mxEvent.UP, this.resetHandler);
+    }
+  }
+
+  /**
+   * Function: setHighlightColor
+   *
+   * Sets the color of the rectangle used to highlight drop targets.
+   *
+   * Parameters:
+   *
+   * color - String that represents the new highlight color.
+   */
+  setHighlightColor = (color) => {
+    this.highlightColor = color;
+
+    if (this.shape != null) {
+      this.shape.stroke = color;
+    }
+  };
+
+  /**
+   * Function: drawHighlight
+   *
+   * Creates and returns the highlight shape for the given state.
+   */
+  drawHighlight = () => {
+    this.shape = this.createShape();
+    this.repaint();
+
+    if (!this.keepOnTop && this.shape.node.parentNode.firstChild != this.shape.node) {
+      this.shape.node.parentNode.insertBefore(this.shape.node, this.shape.node.parentNode.firstChild);
+    }
+  };
+
+  /**
+   * Function: createShape
+   *
+   * Creates and returns the highlight shape for the given state.
+   */
+  createShape = () => {
+    var shape = this.graph.cellRenderer.createShape(this.state);
+
+    shape.svgStrokeTolerance = this.graph.tolerance;
+    shape.points = this.state.absolutePoints;
+    shape.apply(this.state);
+    shape.stroke = this.highlightColor;
+    shape.opacity = this.opacity;
+    shape.isDashed = this.dashed;
+    shape.isShadow = false;
+
+    shape.dialect = (this.graph.dialect != mxConstants.DIALECT_SVG) ? mxConstants.DIALECT_VML : mxConstants.DIALECT_SVG;
+    shape.init(this.graph.getView().getOverlayPane());
+    mxEvent.redirectMouseEvents(shape.node, this.graph, this.state);
+
+    if (this.graph.dialect != mxConstants.DIALECT_SVG) {
+      shape.pointerEvents = false;
+    } else {
+      shape.svgPointerEvents = 'stroke';
+    }
+
+    return shape;
+  };
+
+  /**
+   * Function: getStrokeWidth
+   *
+   * Returns the stroke width.
+   */
+  getStrokeWidth = (state) => {
+    return this.strokeWidth;
+  };
+
+  /**
+   * Function: repaint
+   *
+   * Updates the highlight after a change of the model or view.
+   */
+  repaint = () => {
+    if (this.state != null && this.shape != null) {
+      this.shape.scale = this.state.view.scale;
+
+      if (this.graph.model.isEdge(this.state.cell)) {
+        this.shape.strokewidth = this.getStrokeWidth();
+        this.shape.points = this.state.absolutePoints;
+        this.shape.outline = false;
+      } else {
+        this.shape.bounds = new mxRectangle(this.state.x - this.spacing, this.state.y - this.spacing,
+            this.state.width + 2 * this.spacing, this.state.height + 2 * this.spacing);
+        this.shape.rotation = Number(this.state.style[mxConstants.STYLE_ROTATION] || '0');
+        this.shape.strokewidth = this.getStrokeWidth() / this.state.view.scale;
+        this.shape.outline = true;
       }
-    });
 
-    this.graph.getView().addListener(mxEvent.SCALE, this.repaintHandler);
-    this.graph.getView().addListener(mxEvent.TRANSLATE, this.repaintHandler);
-    this.graph.getView().addListener(mxEvent.SCALE_AND_TRANSLATE, this.repaintHandler);
-    this.graph.getModel().addListener(mxEvent.CHANGE, this.repaintHandler);
-    
-    // Hides the marker if the current root changes
-    this.resetHandler = mxUtils.bind(this, ()=>{
-    {
-      this.hide();
-    });
+      // Uses cursor from shape in highlight
+      if (this.state.shape != null) {
+        this.shape.setCursor(this.state.shape.getCursor());
+      }
 
-    this.graph.getView().addListener(mxEvent.DOWN, this.resetHandler);
-    this.graph.getView().addListener(mxEvent.UP, this.resetHandler);
-  }
-};
-
-/**
- * Variable: keepOnTop
- * 
- * Specifies if the highlights should appear on top of everything
- * else in the overlay pane. Default is false.
- */
-keepOnTop = false;
-
-/**
- * Variable: graph
- * 
- * Reference to the enclosing <mxGraph>.
- */
-graph = null;
-
-/**
- * Variable: state
- * 
- * Reference to the <mxCellState>.
- */
-state = null;
-
-/**
- * Variable: spacing
- * 
- * Specifies the spacing between the highlight for vertices and the vertex.
- * Default is 2.
- */
-spacing = 2;
-
-/**
- * Variable: resetHandler
- * 
- * Holds the handler that automatically invokes reset if the highlight
- * should be hidden.
- */
-resetHandler = null;
-
-/**
- * Function: setHighlightColor
- * 
- * Sets the color of the rectangle used to highlight drop targets.
- * 
- * Parameters:
- * 
- * color - String that represents the new highlight color.
- */
-setHighlightColor = (color)=>{
-{
-  this.highlightColor = color;
-  
-  if (this.shape != null)
-  {
-    this.shape.stroke = color;
-  }
-};
-
-/**
- * Function: drawHighlight
- * 
- * Creates and returns the highlight shape for the given state.
- */
-drawHighlight = ()=>{
-{
-  this.shape = this.createShape();
-  this.repaint();
-
-  if (!this.keepOnTop && this.shape.node.parentNode.firstChild != this.shape.node)
-  {
-    this.shape.node.parentNode.insertBefore(this.shape.node, this.shape.node.parentNode.firstChild);
-  }
-};
-
-/**
- * Function: createShape
- * 
- * Creates and returns the highlight shape for the given state.
- */
-createShape = ()=>{
-{
-  var shape = this.graph.cellRenderer.createShape(this.state);
-  
-  shape.svgStrokeTolerance = this.graph.tolerance;
-  shape.points = this.state.absolutePoints;
-  shape.apply(this.state);
-  shape.stroke = this.highlightColor;
-  shape.opacity = this.opacity;
-  shape.isDashed = this.dashed;
-  shape.isShadow = false;
-  
-  shape.dialect = (this.graph.dialect != mxConstants.DIALECT_SVG) ? mxConstants.DIALECT_VML : mxConstants.DIALECT_SVG;
-  shape.init(this.graph.getView().getOverlayPane());
-  mxEvent.redirectMouseEvents(shape.node, this.graph, this.state);
-  
-  if (this.graph.dialect != mxConstants.DIALECT_SVG)
-  {
-    shape.pointerEvents = false;
-  }
-  else
-  {
-    shape.svgPointerEvents = 'stroke';
-  }
-  
-  return shape;
-};
-
-/**
- * Function: getStrokeWidth
- * 
- * Returns the stroke width.
- */
-getStrokeWidth = (state)=>{
-{
-  return this.strokeWidth;
-};
-
-/**
- * Function: repaint
- * 
- * Updates the highlight after a change of the model or view.
- */
-repaint = ()=>{
-{
-  if (this.state != null && this.shape != null)
-  {
-    this.shape.scale = this.state.view.scale;
-    
-    if (this.graph.model.isEdge(this.state.cell))
-    {
-      this.shape.strokewidth = this.getStrokeWidth();
-      this.shape.points = this.state.absolutePoints;
-      this.shape.outline = false;
+      this.shape.redraw();
     }
-    else
-    {
-      this.shape.bounds = new mxRectangle(this.state.x - this.spacing, this.state.y - this.spacing,
-          this.state.width + 2 * this.spacing, this.state.height + 2 * this.spacing);
-      this.shape.rotation = Number(this.state.style[mxConstants.STYLE_ROTATION] || '0');
-      this.shape.strokewidth = this.getStrokeWidth() / this.state.view.scale;
-      this.shape.outline = true;
+  };
+
+  /**
+   * Function: hide
+   *
+   * Resets the state of the cell marker.
+   */
+  hide = () => {
+    this.highlight(null);
+  };
+
+  /**
+   * Function: mark
+   *
+   * Marks the <markedState> and fires a <mark> event.
+   */
+  highlight = (state) => {
+    if (this.state != state) {
+      if (this.shape != null) {
+        this.shape.destroy();
+        this.shape = null;
+      }
+
+      this.state = state;
+
+      if (this.state != null) {
+        this.drawHighlight();
+      }
+    }
+  };
+
+  /**
+   * Function: isHighlightAt
+   *
+   * Returns true if this highlight is at the given position.
+   */
+  isHighlightAt = (x, y) => {
+    var hit = false;
+
+    if (this.shape != null && document.elementFromPoint != null) {
+      var elt = document.elementFromPoint(x, y);
+
+      while (elt != null) {
+        if (elt == this.shape.node) {
+          hit = true;
+          break;
+        }
+
+        elt = elt.parentNode;
+      }
     }
 
-    // Uses cursor from shape in highlight
-    if (this.state.shape != null)
-    {
-      this.shape.setCursor(this.state.shape.getCursor());
-    }
-    
-    this.shape.redraw();
-  }
-};
+    return hit;
+  };
 
-/**
- * Function: hide
- * 
- * Resets the state of the cell marker.
- */
-hide = ()=>{
-{
-  this.highlight(null);
-};
+  /**
+   * Function: destroy
+   *
+   * Destroys the handler and all its resources and DOM nodes.
+   */
+  destroy = () => {
+    this.graph.getView().removeListener(this.resetHandler);
+    this.graph.getView().removeListener(this.repaintHandler);
+    this.graph.getModel().removeListener(this.repaintHandler);
 
-/**
- * Function: mark
- * 
- * Marks the <markedState> and fires a <mark> event.
- */
-highlight = (state)=>{
-{
-  if (this.state != state)
-  {
-    if (this.shape != null)
-    {
+    if (this.shape != null) {
       this.shape.destroy();
       this.shape = null;
     }
-
-    this.state = state;
-    
-    if (this.state != null)
-    {
-      this.drawHighlight();
-    }
   }
-};
+}
 
-/**
- * Function: isHighlightAt
- * 
- * Returns true if this highlight is at the given position.
- */
-isHighlightAt = (x, y)=>{
-{
-  var hit = false;
-  
-  if (this.shape != null && document.elementFromPoint != null)
-  {
-    var elt = document.elementFromPoint(x, y);
-
-    while (elt != null)
-    {
-      if (elt == this.shape.node)
-      {
-        hit = true;
-        break;
-      }
-      
-      elt = elt.parentNode;
-    }
-  }
-  
-  return hit;
-};
-
-/**
- * Function: destroy
- * 
- * Destroys the handler and all its resources and DOM nodes.
- */
-destroy = ()=>{
-{
-  this.graph.getView().removeListener(this.resetHandler);
-  this.graph.getView().removeListener(this.repaintHandler);
-  this.graph.getModel().removeListener(this.repaintHandler);
-  
-  if (this.shape != null)
-  {
-    this.shape.destroy();
-    this.shape = null;
-  }
-};
+export default mxCellHighlight;
--- src/js/handler/mxCellTracker.js
+++ src/js/handler/mxCellTracker.js
@@ -2,135 +2,131 @@
  * Copyright (c) 2006-2015, JGraph Ltd
  * Copyright (c) 2006-2015, Gaudenz Alder
  */
-/**
- * Class: mxCellTracker
- * 
- * Event handler that highlights cells. Inherits from <mxCellMarker>.
- * 
- * Example:
- * 
- * (code)
- * new mxCellTracker(graph, '#00FF00');
- * (end)
- * 
- * For detecting dragEnter, dragOver and dragLeave on cells, the following
- * code can be used:
- * 
- * (code)
- * graph.addMouseListener(
- * {
- *   cell: null,
- *   mouseDown: (sender, me)=> { },
- *   mouseMove: (sender, me)=>
- *   {
- *     var tmp = me.getCell();
- *     
- *     if (tmp != this.cell)
- *     {
- *       if (this.cell != null)
- *       {
- *         this.dragLeave(me.getEvent(), this.cell);
- *       }
- *       
- *       this.cell = tmp;
- *       
- *       if (this.cell != null)
- *       {
- *         this.dragEnter(me.getEvent(), this.cell);
- *       }
- *     }
- *     
- *     if (this.cell != null)
- *     {
- *       this.dragOver(me.getEvent(), this.cell);
- *     }
- *   },
- *   mouseUp: (sender, me)=> { },
- *   dragEnter: (evt, cell)=>
- *   {
- *     mxLog.debug('dragEnter', cell.value);
- *   },
- *   dragOver: (evt, cell)=>
- *   {
- *     mxLog.debug('dragOver', cell.value);
- *   },
- *   dragLeave: (evt, cell)=>
- *   {
- *     mxLog.debug('dragLeave', cell.value);
- *   }
- * });
- * (end)
- * 
- * Constructor: mxCellTracker
- * 
- * Constructs an event handler that highlights cells.
- * 
- * Parameters:
- * 
- * graph - Reference to the enclosing <mxGraph>.
- * color - Color of the highlight. Default is blue.
- * funct - Optional JavaScript function that is used to override
- * <mxCellMarker.getCell>.
- */
-function mxCellTracker(graph, color, funct)
-{
-  mxCellMarker.call(this, graph, color);
 
-  this.graph.addMouseListener(this);
-  
-  if (funct != null)
-  {
-    this.getCell = funct;
-  }
-};
+class mxCellTracker extends mxCellMarker {
+  /**
+   * Class: mxCellTracker
+   *
+   * Event handler that highlights cells. Inherits from <mxCellMarker>.
+   *
+   * Example:
+   *
+   * (code)
+   * new mxCellTracker(graph, '#00FF00');
+   * (end)
+   *
+   * For detecting dragEnter, dragOver and dragLeave on cells, the following
+   * code can be used:
+   *
+   * (code)
+   * graph.addMouseListener(
+   * {
+   *   cell: null,
+   *   mouseDown: (sender, me)=> { },
+   *   mouseMove: (sender, me)=>
+   *   {
+   *     var tmp = me.getCell();
+   *
+   *     if (tmp != this.cell)
+   *     {
+   *       if (this.cell != null)
+   *       {
+   *         this.dragLeave(me.getEvent(), this.cell);
+   *       }
+   *
+   *       this.cell = tmp;
+   *
+   *       if (this.cell != null)
+   *       {
+   *         this.dragEnter(me.getEvent(), this.cell);
+   *       }
+   *     }
+   *
+   *     if (this.cell != null)
+   *     {
+   *       this.dragOver(me.getEvent(), this.cell);
+   *     }
+   *   },
+   *   mouseUp: (sender, me)=> { },
+   *   dragEnter: (evt, cell)=>
+   *   {
+   *     mxLog.debug('dragEnter', cell.value);
+   *   },
+   *   dragOver: (evt, cell)=>
+   *   {
+   *     mxLog.debug('dragOver', cell.value);
+   *   },
+   *   dragLeave: (evt, cell)=>
+   *   {
+   *     mxLog.debug('dragLeave', cell.value);
+   *   }
+   * });
+   * (end)
+   *
+   * Constructor: mxCellTracker
+   *
+   * Constructs an event handler that highlights cells.
+   *
+   * Parameters:
+   *
+   * graph - Reference to the enclosing <mxGraph>.
+   * color - Color of the highlight. Default is blue.
+   * funct - Optional JavaScript function that is used to override
+   * <mxCellMarker.getCell>.
+   */
+  constructor(graph, color, funct) {
+    super(graph, color);
 
-/**
- * Extends mxCellMarker.
- */
-mxUtils.extend(mxCellTracker, mxCellMarker);
+    this.graph.addMouseListener(this);
 
-/**
- * Function: mouseDown
- * 
- * Ignores the event. The event is not consumed.
- */
-mouseDown = (sender, me)=> { };
+    if (funct != null) {
+      this.getCell = funct;
+    }
+  };
 
-/**
- * Function: mouseMove
- * 
- * Handles the event by highlighting the cell under the mousepointer if it
- * is over the hotspot region of the cell.
- */
-mouseMove = (sender, me)=>
-{
-  if (this.isEnabled())
-  {
-    this.process(me);
-  }
-};
+  /**
+   * Function: mouseDown
+   *
+   * Ignores the event. The event is not consumed.
+   */
+  mouseDown = (sender, me) => {
+  };
 
-/**
- * Function: mouseUp
- * 
- * Handles the event by reseting the highlight.
- */
-mouseUp = (sender, me)=> { };
+  /**
+   * Function: mouseMove
+   *
+   * Handles the event by highlighting the cell under the mousepointer if it
+   * is over the hotspot region of the cell.
+   */
+  mouseMove = (sender, me) => {
+    if (this.isEnabled()) {
+      this.process(me);
+    }
+  };
 
-/**
- * Function: destroy
- * 
- * Destroys the object and all its resources and DOM nodes. This doesn't
- * normally need to be called. It is called automatically when the window
- * unloads.
- */
-destroy = ()=>
-{
-  if (!this.destroyed)
-  {
-    this.destroyed = true;
+  /**
+   * Function: mouseUp
+   *
+   * Handles the event by reseting the highlight.
+   */
+  mouseUp = (sender, me) => {
+  };
 
-    this.graph.removeMouseListener(this);
-    destroy.apply(this);
-  }
-};
+  /**
+   * Function: destroy
+   *
+   * Destroys the object and all its resources and DOM nodes. This doesn't
+   * normally need to be called. It is called automatically when the window
+   * unloads.
+   */
+  destroy = () => {
+    if (!this.destroyed) {
+      this.destroyed = true;
+
+      this.graph.removeMouseListener(this);
+      destroy.apply(this);
+    }
+  };
+}
+
+export default mxCellTracker;
