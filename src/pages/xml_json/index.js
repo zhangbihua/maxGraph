3b3799426c4f12d141e8ff63d517cbf7a6a74605
mcyph <20507948+mcyph@users.noreply.github.com>
2021-04-11 11:18:03 +1000

    removed resources example

--- src/pages/xml_json/index.js
+++ src/pages/xml_json/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Preview from '../Previews';
 import JsonData from './JsonData';
-import Resources from './Resources';
 import UserObject from './UserObject';
 import PageTabs from '../PageTabs';
 
@@ -9,7 +8,6 @@ export default function _XMLJSON() {
   return (
     <PageTabs curPageURL="/xml_json">
       <Preview sourceKey="JsonData" content={<JsonData />} />
-      <Preview sourceKey="Resources" content={<Resources />} />
       <Preview sourceKey="UserObject" content={<UserObject />} />
     </PageTabs>
   );
