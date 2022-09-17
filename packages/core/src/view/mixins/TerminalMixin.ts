985fc7513994444d54e47bab4dec988d726c5fe1
Junsik Shim <junsik.shim@gmail.com>
2022-09-17 19:08:41 +0900

    Remove CellArray leftovers.

--- packages/core/src/view/mixins/TerminalMixin.ts
+++ packages/core/src/view/mixins/TerminalMixin.ts
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import CellArray from '../cell/CellArray';
 import Cell from '../cell/Cell';
 import Dictionary from '../../util/Dictionary';
 import { Graph } from '../Graph';
