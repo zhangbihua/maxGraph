27f75a59f3728ff2e592982121cdda4b7d36073a
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-11-07 18:31:40 +0100

    docs: set the minimum TypeScript requirement to 3.8 (#128)
    We use a recent TypeScript version to build maxGraph but applications
    that integrates it may be forced to use old TypeScript versions.
    We never explain what is the minimum TypeScript version requires to use
    `maxGraph`. The changes proposed here are a first step to make this
    information available.
    It is only informative for now (mentioned in the README) and the minimum
    TypeScript version is tested in the `ts-support` package (part of the
    'build' GitHub workflow).

---
+++ packages/ts-support/src/index.ts
@@ -0,0 +1,27 @@
+/*
+Copyright 2022-present The maxGraph project Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { Client, Graph, InternalEvent } from '@maxgraph/core';
+
+const version = Client.VERSION;
+
+// Creates the graph inside the given container
+const container = <HTMLElement>document.getElementById('graph-container');
+// Disables the built-in context menu
+InternalEvent.disableContextMenu(container);
+
+const graph = new Graph(container);
+graph.setPanning(true); // Use mouse right button for panning
